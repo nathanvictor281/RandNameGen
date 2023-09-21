46ac4e9de71eb8bf443cc67037a4fb2fe4f3a155
trapi2009 <78277072+trapi2009@users.noreply.github.com>
2023-09-21 16:08:59 -0700

    Added ; to sketch.js

--- sketch.js
+++ sketch.js
@@ -25,9 +25,9 @@ function addNames() {
 function displayNames() {
   shuffle(names, true);
   nameList.html("");
-  document.getElementById("reshuffleButton").style.display = "block"
   for (let i = 0; i < names.length; i++) {
     let listItem = createElement("li", `${i + 1}. ${names[i]}`);
     listItem.parent(nameList);
   }
+  document.getElementById("reshuffleButton").style.display = "block";
 }
